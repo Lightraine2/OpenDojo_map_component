420aaec3aa80a4beeaef5f69b9aff2bc2cb7eaf9
Lightraine2 <laurencedale1986@gmail.com>
2021-02-08 08:58:11 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Market,
+  InfoWindow
+} from "@react-google-maps/api";
+import {formatRelative} from "date-fns";
+import "@reach/combobox/styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapStyles from './mapStyles';
+
+require('dotenv').config();
+
+const libraries= ["places"];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
 }
 
-export default App;
+const center = {
+  lat: 51.538120,
+  lng: -2.151510,
+
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const [markets, setMarkers] = React.useState([]);
+
+if (loadError) return "Error Loading Maps";
+if (!isLoaded) return "Loading Maps";
+
+  //console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+  //console.log(mapStyles);
+  return <div>
+    <GoogleMap mapContainerStyle={mapContainerStyle}
+     zoom={12} 
+     center={center}
+     options={options}
+     onClick={(event) => {
+       //this is dropping a pin
+       setMarkers(current => [
+         ...current,
+         {
+           lat: event.latLng.lat(),
+           lng: event.latLng.lng(),
+           time: new Date(),
+         },
+       ]);
+     }}
+     >
+
+     </GoogleMap>
+  </div>;
+}
+
+
